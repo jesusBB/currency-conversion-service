f49dbded593b06825c1f6652e005f82a991b2966
Jesus Bustillo <jesus.bustillo@cdl.co.uk>
2020-06-09 15:22:39 +0100

    Adding feign and ribbon

--- currency-conversion-service/src/test/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
+++ currency-conversion-service/src/test/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -1,13 +1,13 @@
 package com.in28minutes.microservices.currencyconversionservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CurrencyConversionServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
